ec91abfe0bf757ac24a3da62e7f318c1acc1d68d
khoatrando516 <khoatrando516@gmail.com>
2020-09-04 00:57:15 +0700

    Update them course vao database

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,5 +10,22 @@ class CourseController {
         })
         .catch(next);
   }
+  //[GET]ourses/create"
+  create(req, res, next) {
+      res.render("courses/create")
+  }
+  //[POST] courses/create/store
+  store(req, res, next) {
+    const formdata = req.body;
+    formdata.image= `https://img.youtube.com/vi/${req.body.videoId}/default.jpg`
+    const course=new Course(req.body);
+    course.save()
+    .then(() => res.redirect(`/`))
+    .catch(error =>{
+
+    })
+
+    res.send("Course save")
+}
 }
 module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,20 @@
-
+//ding nghia du lieu
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Course = new Schema({
   name: {type : String, maxlength:255},
-  discription: {type :String,maxlength:600},
+  description: {type :String},
   image: {type: String, maxlength:255},
-  createAt: { type:Date, default: Date.now},
-  updateAt: { type:Date, default: Date.now},
+  videoId: {type: String, maxlength:255},
+  level: {type: String, maxlength:255},
+  slug: { type: String, slug: 'name',unique:true}
+},{
+  timestamps:true,
 });
 
 module.exports = mongoose.model('Course',Course);
